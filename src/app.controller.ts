ca02e4ba8035ace3cc9ba5416fe376e4125ea54b
Jakub Sokolowski <jrsokolow@gmail.com>
2022-03-31 22:45:19 +0200

    test post with body using DTO

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, ConsoleLogger } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserDTO } from './dto/user.dto';
 
-@Controller()
+@Controller("main")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post("user")
+  saveUser(@Body() userDto: UserDTO) {
+    console.log(userDto.id);
+    console.log(userDto.username);
   }
 }
